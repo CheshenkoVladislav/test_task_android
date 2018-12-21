57a0116f07a89e9446283a5aad6ebf1db49edbd3
VladislavCheshenko <epotajni1488@gmail.com>
2018-12-21 16:36:24 +0300

    refactor

--- app/src/main/java/com/example/vlad/alphatest/model/BaseModel.java
+++ app/src/main/java/com/example/vlad/alphatest/model/BaseModel.java
@@ -11,22 +11,22 @@ import io.reactivex.ObservableOnSubscribe;
 
 import io.reactivex.schedulers.Schedulers;
 
-public abstract class BaseModel {
+abstract class BaseModel {
     private ThreadExecutor threadExecutor;
     private PostExecutionThread postExecutionThread;
     private int timeoutSec = 20;
 
 
-    public BaseModel(ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
+    BaseModel(ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
         this.threadExecutor = threadExecutor;
         this.postExecutionThread = postExecutionThread;
     }
 
-    protected <R extends RxResult> Observable<R> buildObservable(ObservableOnSubscribe<R> emitter) {
+    <R extends RxResult> Observable<R> buildObservable(ObservableOnSubscribe<R> emitter) {
         return Observable.create(emitter).subscribeOn(postExecutionThread.getScheduler()).timeout(timeoutSec, TimeUnit.SECONDS);
     }
 
-    protected <R> Observable<R> buildAsyncObservable(ObservableOnSubscribe<R> emitter) {
+    <R> Observable<R> buildAsyncObservable(ObservableOnSubscribe<R> emitter) {
         return Observable.create(emitter).subscribeOn(Schedulers.from(threadExecutor))
                 .observeOn(postExecutionThread.getScheduler()).timeout(timeoutSec, TimeUnit.SECONDS);
     }
